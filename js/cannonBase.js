930fa4ef8f22d37ab3c9368390632bbcff6d9b45
pedro-lamego <pedrownlamego@hotmail.com>
2019-10-14 13:56:26 +0100

    Rodas novas

--- js/cannonBase.js
+++ js/cannonBase.js
@@ -3,11 +3,11 @@ function createCannonBase(obj, x, y, z){
 
     var base = new THREE.Group();
 
-    createBottomBase(base, 0, 0, 0); // base referencial origin
-    createWallBase(base, 0, 2, 7);
-    createWallBase(base, 0, 2, -7);
-    createWheel(base, -1, -4, 8.5);
-    createWheel(base, -1, -4, -8.5);
+    createBottomBase(base, -3, 0, 0); // base referencial origin
+    createWallBase(base, -3, 2, 7);
+    createWallBase(base, -3, 2, -7);
+    createWheel(base, -4, -4, 8.5);
+    createWheel(base, -4, -4, -8.5);
 
     base.position.set(x, y, z);
 
@@ -40,12 +40,36 @@ function createWallBase(obj, x, y, z) {
 
 function createWheel(obj, x, y, z) {
     'use strict';
+    var wheel = new THREE.Group();
+    createWheelTorus(wheel, 0 , 0, 0);
+    createWheelCylinder(wheel, 0, 0, 0);
+    wheel.position.set(x, y, z);
+    obj.add(wheel);
+    return wheel;
+
+}
+
+function createWheelTorus(obj, x, y, z){
+	'use strict';
 
     var material = new THREE.MeshBasicMaterial({color: '#4b3832', wireframe: true});
-    var geometry = new THREE.CylinderGeometry(6.5, 6.5, 1, 20, 30);
+    var geometry = new THREE.TorusGeometry(6.5, 1.5, 10, 50, 6.3);
     var mesh = new THREE.Mesh(geometry, material);
-    mesh.rotation.set(-Math.PI/2, 0, 0);
     mesh.position.set(x, y, z);
     obj.add(mesh);
+}
 
+function createWheelCylinder(obj, x, y, z) {
+	'use strict';
+	var angle = 0;
+	var i;
+	for (i = 0; i< 4; i++){	
+		var material = new THREE.MeshBasicMaterial({color: '#4b3832', wireframe: true});
+   		var geometry = new THREE.CylinderGeometry(0.5, 0.5, 13, 20, 20);
+    	var mesh = new THREE.Mesh(geometry, material);
+    	mesh.rotation.set(0, 0, angle);
+    	angle += Math.PI/4;
+    	mesh.position.set(x, y, z);
+    	obj.add(mesh);
+    }
 }
