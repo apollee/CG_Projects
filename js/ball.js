b9e21a06938b1f2e366cd2552483d790d0a45304
AfonsoRibeiro <1998afonsoribeiro1998@gmail.com>
2019-10-19 15:42:41 +0100

    Make axis visible and invisible

--- js/ball.js
+++ js/ball.js
@@ -1,23 +1,33 @@
 class Ball extends THREE.Object3D { /* lacks attributes such as direction */
 
-	constructor(x, y, z) {
+	constructor(x, y, z, vectorDir, vectorRot) {
         'use strict';
 		
         super();
+        
+        this.axis = new THREE.AxisHelper(5);
+
+        this.add(this.axis);
 
         var material = new THREE.MeshBasicMaterial({color: '#be9b7b', wireframe: true});
         var geometry = new THREE.SphereGeometry(3, 16, 16);
         var mesh = new THREE.Mesh(geometry, material);
+
 		this.add(mesh);
 	
 		this.position.set(x, y, z);
 	}
 
-    showHideAxis() {
-        
+    axisShowHide() {
+        this.axis.visible = !this.axis.visible;
+    }
+
+    move() {
+
     }
 
     update() {  /* needs to check if its out of bounds and colisions */
+        this.move();
         return true;
     }
 }
